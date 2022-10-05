d3e2fe24392993ce14c0cabeb275101854065c7c
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-05 17:01:10 +0300

    Решили задачу 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -13,4 +13,11 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число ");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
-int max = number1
+int max = number1;
+{
+if (number2 > max)
+        max = number2;
+if (number3 > max)
+        max = number3;
+}
+Console.WriteLine ($"Максимальное число из трех = {max}");
