39e1d853b80049dc367aba46623c6683aa502231
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-06 09:43:44 +0300

    Решили задачу 8

---
+++ Task8/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите любое число: "); 
+int numberN = Convert.ToInt32(Console.ReadLine()); 
+int startnumber = 1; 
+ 
+while (startnumber <= numberN) 
+{ 
+    if (startnumber % 2 == 0) 
+{ 
+    Console.WriteLine($"Четное число:{startnumber}"); 
+    startnumber +=2; 
+} 
+else  
+{ 
+startnumber++; 
+}
+}
+
+
+
+
