e11580fe34ec14e906eff94e3c3ec2d60953aa4d
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-06 09:49:28 +0300

    Проверка задач

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,14 +1,13 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
 //какое число большее, а какое меньшее.
-
 //a = 5; b = 7 -> max = 7
 //a = 2; b = 10 -> max = 10
 //a = -9; b = -3 -> max = -3
 
-Console.WriteLine("Введите первое число а  ");
+Console.WriteLine("Введите первое число А: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число b  ");
+Console.WriteLine("Введите второе число B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
 int max = numberA;
