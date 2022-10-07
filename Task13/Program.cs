b0321f6ff980034ba11c164421c3bdfaf0094774
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-07 14:29:51 +0300

    Решение 13 задачи

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,14 +1,17 @@
-﻿Console.WriteLine("Введите число: ");
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+//Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int nums = num;
 if (num < 100)
-{
+
     Console.WriteLine("Третьей цифры нет");
-}
+
 else while (num > 1000)
-    {
-        num = num / 10;
-    }
+
+    num = num / 10;
+    
 if ((num < 1000) && (num > 99)) 
 Console.WriteLine($"Третья цифра числа {nums}: {num % 10}");
 
