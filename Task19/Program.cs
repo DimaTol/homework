86a796897a68f6ac4db78a784225bf13f6a06f88
DimaTol <114100410+DimaTol@users.noreply.github.com>
2022-10-18 13:48:58 +0300

    Задача про Палиндром

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int number = Convert.ToInt32(Console.ReadLine());
+int lenght = number.ToString().Length;
+string str = number.ToString();
+int i = 0;
+int j = 0;
+
+for (i = 0; i < lenght / 2; i++)
+if (str[i] == str[lenght - 1 - i])
+{
+j++;
+}
+if (j == lenght / 2)
+{
+    Console.WriteLine("Введенное число палиндром");
+}
+else
+{
+    Console.WriteLine("Введенное число не является палиндромом");
+}
